f01bdfcf116e9e84b52698480a440e527a210dfa
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-02-01 11:14:52 +0530

    inline functions

---
+++ week 02/day 3/inline.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+// use when the size of the function is small
+inline void print( int n){
+    cout<<" number = "<<n<<endl;
+    cout<<" number * 2 = "<<2*n<<endl<<endl;
+}
+
+int main(){
+
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    print(10);
+    
+    return 0;
+}
